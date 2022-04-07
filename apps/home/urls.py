71be1a187c0293a1791e68f4b65c8f8251738c06
Sarra Mansour <sarramansour321@gmail.ocm>
2022-04-07 18:58:58 +0100

    add_user_button

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -5,11 +5,13 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.urls import path, re_path
 from apps.home import views
-
+from .views import add_user
+from django.views.generic import TemplateView
 urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
+    re_path('add_user', add_user, name="add_user"),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
--- apps/home/views.py
+++ apps/home/views.py
@@ -8,7 +8,11 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.shortcuts import render
 
+def add_user(request):
+    # return response
+    return render(request, "home/AjouterUtilisatreur.html")
 
 @login_required(login_url="/login/")
 def index(request):
